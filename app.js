0960abd07614dfe0804569d2df2d40a3eb44cf6c
Ryan Milne <ryanmilne101@gmail.com>
2020-10-22 15:07:48 -0600

    learned about console.logs

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-alert('Hello World')
+// Log to console
+console.log('Hello World')
+console.log(123)
+console.log(true)
+var greeting = 'Hello'
+console.log(greeting)
+console.log([1,2,3,4,5,6])
+console.log({a:1, b:2})
+console.table({a:1, b:2})
+
+console.error("This is some error")
+console.clear()
+console.warn('this is a warning')
+console.time('hello')
+console.log('Hello World')
+console.log('Hello World')
+console.log('Hello World')
+console.timeEnd('hello')
