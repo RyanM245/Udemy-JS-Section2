f035a0833f097cef0f5447e1f69546478e2fee77
Ryan Milne <ryanmilne101@gmail.com>
2020-10-23 17:23:50 -0600

    refresh on let and const

--- app.js
+++ app.js
@@ -16,5 +16,37 @@
 // console.log('Hello World')
 // console.log('Hello World')
 // console.timeEnd('hello')
+//////////step 7/////////
+// let name = 'Ryan Milne';
+// console.log(name)
+// name = 'Kevin'
+// console.log(name)
 
+// let greeting 
+// console.log(greeting)
+// greeting= 'hello'
+// console.log(greeting)
+// //letters,number,_, $
+// //can not start with a number
+// let firstName = 'Ryan'//Camel case
+// let last_name = 'Milne'//underscore
+// let My_name = 'Ryan'//Pascal case 
+// let thename;
 
+// CONST
+// const name = "Ryan"
+// console.log(name)
+// // name = 'Kevin'
+
+// const person = {
+//   name: 'Ryan',
+//   age: 24
+// }
+// person.name = 'Milne'
+// person.age = 30
+// // console.log(person)
+
+// const num = [1,2,3,4,5,6,7,8,9]
+// console.log(num)
+// num.push(10)
+// console.log(num)
