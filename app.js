814757e20fa63103a20a45b97c5cd79bd8d2ec3a
Ryan Milne <ryanmilne101@gmail.com>
2020-10-27 17:59:18 -0600

    Type Conversion

--- app.js
+++ app.js
@@ -81,3 +81,44 @@
 // // console.log(today)
 
 // console.log(typeof address)
+
+// type conversion //
+// let val;
+
+// // Num to string
+// val = String(555);
+// val = String(4+4)
+// //Bool to string
+// val = String(true)
+// //date to  string
+// val = String(new Date())
+// //array to sting
+// val = String([1,2,3,4])
+
+// toString()
+// val = (5).toString()
+// val = (true).toString()
+
+// string to num
+// val = Number("5")
+// val = Number(true)
+// val = Number(null)
+// val = Number(false)
+// val = Number('hello')
+// val = Number([1,2,3])
+
+// val = parseInt('100.30')
+// val = parseFloat('100.30')
+// Output
+// console.log(val)
+// console.log(typeof val)
+// // console.log(val.length)
+// console.log(val.toFixed())
+
+// const val1 = '5';
+// const val2 = 6;
+// const sum = Number(val1 + val2)
+
+// console.log(sum)
+// console.log(typeof sum)
+
